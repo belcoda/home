27ac0399e7aab4592b7a1a89bc6d1d287790d272
Django Merope <dmerope@gmail.com>
2025-11-07 17:42:19 +0900

    Blog layout

---
+++ src/lib/layouts/blog.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+	import Header from '$lib/comps/nav/Header.svelte';
+	import Footer from '$lib/comps/nav/Footer.svelte';
+	import { type Snippet } from 'svelte';
+	type Props = {
+		title: string;
+		author: string;
+		date: string;
+		description: string;
+		author_avatar: string;
+		author_title: string;
+		tag: string;
+		image?: string;
+		darken_image?: boolean;
+		children: Snippet;
+	};
+	const {
+		title,
+		author,
+		date,
+		description,
+		author_avatar,
+		author_title,
+		tag,
+		image,
+		children,
+		darken_image = false
+	}: Props = $props();
+
+	const backgroundImage = image || '/background-auth.jpg';
+</script>
+
+<div
+	class="relative flex h-100 w-full items-end justify-start bg-linear-to-b from-blue-200 to-white bg-cover bg-center bg-no-repeat"
+	style="background-image: url('{backgroundImage}')"
+>
+	<Header />
+
+	<div class="z-10 mx-auto w-full max-w-5xl px-4">
+		<!-- Tag -->
+		<div class="lg:mb-2">
+			<span
+				class="relative z-10 rounded-full bg-white/20 px-3 py-1.5 text-sm font-medium text-white/70"
+				>{tag}</span
+			>
+		</div>
+
+		<!-- Title -->
+		<h1 class="text-4xl font-extrabold tracking-tight text-balance text-white">{title}</h1>
+		<!-- Date and Author -->
+		<div class="mt-1 flex flex-wrap items-center gap-y-1 overflow-hidden text-sm/6 text-gray-200">
+			<time datetime="2020-03-10" class="mr-8">Mar 10, 2020</time>
+			<div class="-ml-4 flex items-center gap-x-4">
+				<svg viewBox="0 0 2 2" class="-ml-0.5 size-0.5 flex-none fill-white/50">
+					<circle r="1" cx="1" cy="1" />
+				</svg>
+				<div class="flex gap-x-2.5">
+					<img src={author_avatar} alt="" class="size-6 flex-none rounded-full bg-white/10" />
+					{author}
+				</div>
+			</div>
+		</div>
+		<!-- Description -->
+		<p class="mt-2 mb-8 text-lg/8 text-pretty text-white/70">{description}</p>
+	</div>
+	{#if darken_image}
+		<div class="absolute inset-0 bg-black/50"></div>
+	{/if}
+</div>
+
+<div class="mx-auto prose mt-12 mb-20 max-w-5xl px-4">{@render children?.()}</div>
+
+<Footer />
