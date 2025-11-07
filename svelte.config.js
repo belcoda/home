bddf61efc96999b512eab51adac52f06dbf15267
Django Merope <dmerope@gmail.com>
2025-11-07 17:42:38 +0900

    Config the markdown plugin

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,22 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
+import { join, dirname } from 'path';
+const __dirname = dirname(new URL(import.meta.url).pathname);
+//layouts
+const pathToBlogLayout = join(__dirname, './src/lib/layouts/blog.svelte');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			layout: {
+				blog: pathToBlogLayout
+			}
+		})
+	],
 	kit: { adapter: adapter() },
 	extensions: ['.svelte', '.svx']
 };
