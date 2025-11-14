48183a22f7e5ad6d1be9934b7e88821c894c015e
Django Merope <dmerope@gmail.com>
2025-11-14 18:44:29 +0900

    Update blog layout

--- src/lib/layouts/blog.svelte
+++ src/lib/layouts/blog.svelte
@@ -28,17 +28,19 @@
 	}: Props = $props();
 
 	const backgroundImage = image || '/background-auth.jpg';
+	import BlogBreadcrumbs from '$lib/comps/blog/BlogBreadcrumbs.svelte';
+	import { slugify } from '$lib/utils';
 </script>
 
 <div
-	class="relative flex h-100 w-full items-end justify-start bg-linear-to-b from-blue-200 to-white bg-cover bg-center bg-no-repeat"
+	class="relative flex h-120 w-full items-end justify-start bg-linear-to-b from-blue-200 to-white bg-cover bg-center bg-no-repeat"
 	style="background-image: url('{backgroundImage}')"
 >
 	<Header />
 
-	<div class="z-10 mx-auto w-full max-w-5xl px-4">
+	<div class="z-10 mx-auto w-full max-w-[65ch] px-4">
 		<!-- Tag -->
-		<div class="lg:mb-2">
+		<div class="lg:mb-4">
 			<span
 				class="relative z-10 rounded-full bg-white/20 px-3 py-1.5 text-sm font-medium text-white/70"
 				>{tag}</span
@@ -46,9 +48,11 @@
 		</div>
 
 		<!-- Title -->
-		<h1 class="text-4xl font-extrabold tracking-tight text-balance text-white">{title}</h1>
+		<h1 class="text-4xl font-extrabold tracking-tight text-balance text-white lg:text-5xl">
+			{title}
+		</h1>
 		<!-- Date and Author -->
-		<div class="mt-1 flex flex-wrap items-center gap-y-1 overflow-hidden text-sm/6 text-gray-200">
+		<div class="mt-4 flex flex-wrap items-center gap-y-1 overflow-hidden text-sm/6 text-gray-200">
 			<time datetime={date} class="mr-8"
 				>{new Date(date).toLocaleDateString('en-US', {
 					year: 'numeric',
@@ -57,7 +61,7 @@
 				})}</time
 			>
 			<div class="-ml-4 flex items-center gap-x-4">
-				<svg viewBox="0 0 2 2" class="-ml-0.5 size-0.5 flex-none fill-white/50">
+				<svg viewBox="0 0 2 2" class="-ml-0.5 size-1 flex-none fill-white/50">
 					<circle r="1" cx="1" cy="1" />
 				</svg>
 				<div class="flex items-center gap-x-2.5">
@@ -76,13 +80,18 @@
 			</div>
 		</div>
 		<!-- Description -->
-		<p class="mt-2 mb-8 text-lg/8 text-pretty text-white/70">{description}</p>
+		<p class="mt-4 mb-8 text-lg/8 text-pretty text-gray-200">{description}</p>
 	</div>
 	{#if darken_image}
 		<div class="absolute inset-0 bg-black/50"></div>
 	{/if}
 </div>
 
-<div class="mx-auto prose mt-12 mb-20 max-w-5xl px-4">{@render children?.()}</div>
+<div class="mx-auto mb-20 max-w-[65ch] px-4">
+	<div class="mt-6 mb-8"><BlogBreadcrumbs {title} href={`/blog/${slugify(title)}`} /></div>
+	<div class="prose prose-lg max-w-[65ch]">
+		{@render children?.()}
+	</div>
+</div>
 
 <Footer />
